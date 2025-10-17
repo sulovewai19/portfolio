ca6769e7ec5b15e17d9f951e9579c3c11cb1b780
Su Love Wai <lwai@ucsd.edu>
2025-10-17 12:59:59 -0700

    Name update

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ let pages = [
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "/"
-    : "/website/";
+    : "/portfolio/";
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
